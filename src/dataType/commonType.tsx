2fd91da95be32c371645ecf98a697e659b2968b2
jbv2 <j4petv2@gmail.com>
2023-04-18 13:39:14 +0900

    39.

---
+++ src/dataType/commonType.tsx
@@ -0,0 +1,13 @@
+export interface ExpensesParam {
+  id: string
+  title: string
+  amount: number
+  date: Date
+}
+
+export interface ExpensesDate {
+  id: string
+  title: string
+  amount: number
+  date: Date
+}
